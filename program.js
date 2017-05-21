679805872f8a278054e3a405c1c46bab6f16098c
Shi Hung <c.shihhung@gmail.com>
2017-05-21 14:40:28 +0000

    Completed lesson two

--- program.js
+++ program.js
@@ -1,1 +1,16 @@
+
+/***This is the lesson one***/
+/*
 console.log("HELLO WORLD");
+*/
+
+/***This is the lesson two***/
+/*
+var sum = 0;
+for(var i = 2; i < process.argv.length; ++i ){
+    sum += Number(process.argv[i]);
+}
+console.log(sum);
+*/
+
+/***This is the lesson three***/
