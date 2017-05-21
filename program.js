1424c29539e3e117503de02d0c51ed577cf88a5d
Shi Hung <c.shihhung@gmail.com>
2017-05-21 15:14:57 +0000

    Completed lesson four

--- program.js
+++ program.js
@@ -14,3 +14,25 @@ console.log(sum);
 */
 
 /***This is the lesson three***/
+/*
+var fs = require('fs');
+var buf = fs.readFileSync(process.argv[2]);
+var string = buf.toString();
+var arr = string.split('\n');
+console.log(arr.length - 1);
+*/
+
+/***This is the lesson four***/
+/*
+var fs = require('fs');
+function logAnswer(){
+    fs.readFile(process.argv[2], function readDone(err, Contents){
+        
+        if(err) throw err;
+        
+        var lines = Contents.toString().split('\n').length - 1;
+        console.log(lines);
+    })
+}
+logAnswer();
+*/
